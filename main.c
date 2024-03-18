1f069930b8049061ccb27349022e4cd5e427bf0a
Izak Halseide <imitatio-ignotum@protonmail.com>
2024-03-18 14:38:49 -0600

    Add 3D camera movement

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "raylib/raylib.h"
 #include "types.h"
+#include "stb_ds.h"
+
+
+static FaceCraft state;
 
 
 void globalCleanup() {
@@ -12,11 +16,53 @@ int main() {
     const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "FaceCraft");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    // Zero-initialize the whole game state
+    memset(&state, 0, sizeof(state));
+
+    // Init camera
+    state.camera.position = (Vector3){ 0.0f, 2.0f, 0.0f };
+    state.camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    state.camera.target = (Vector3){ 0.0f, 2.0f, 4.0f };
+    state.camera.fovy = 70.0f;
+    state.camera.projection = CAMERA_PERSPECTIVE;
+
+    // Prepare to enter game loop
+    DisableCursor();
+
+    float lookXSpeed = 0.1f;
+    float lookYSpeed = 0.1f;
+
     // Main game loop
-    while (!WindowShouldClose()) {    // Detect window close button or ESC key
+    while (!WindowShouldClose()) {
+
+        // Do camera movement update
+        UpdateCameraPro(&state.camera,
+            (Vector3){
+                (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
+                (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,    
+                (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
+                (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
+                0.0f                                                // Move up-down
+            },
+            (Vector3){
+                GetMouseDelta().x*lookXSpeed,                            // Rotation: yaw
+                GetMouseDelta().y*lookYSpeed,                            // Rotation: pitch
+                0.0f                                                // Rotation: roll
+            },
+            0.0f); 
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello! --FaceCraft", 190, 200, 20, LIGHTGRAY);
+        {
+            ClearBackground(RAYWHITE);
+            BeginMode3D(state.camera);
+            {
+                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, BLUE);
+                DrawCube((Vector3){16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);
+            }
+            EndMode3D();
+            DrawText("Hello! --FaceCraft", 190, 200, 20, LIGHTGRAY);
+        }
         EndDrawing();
     }
     globalCleanup();
--- types.h
+++ types.h
@@ -1,12 +1,34 @@
+#ifndef _FACECRAFT_TYPES_H_INCLUDED_
+#define _FACECRAFT_TYPES_H_INCLUDED_
+
+
+#include "raylib/raylib.h"
+
+
 typedef struct CubeDirection {
     int d;
 } CubeDirection;
 
-typedef struct MaterialKind {
-    int k;
-} MaterialKind;
+typedef struct PanelKind {
+    int id;
+} PanelKind;
+
+typedef struct BlockKind {
+    int id;
+} BlockKind;
+
+typedef struct Block {
+    BlockKind kind;
+} Block;
 
 typedef struct Panel {
     CubeDirection dir;
-    MaterialKind mat;
+    PanelKind kind;
 } Panel;
+
+typedef struct FaceCraft {
+    Camera camera;
+} FaceCraft;
+
+
+#endif // _FACECRAFT_TYPES_H_INCLUDED_
