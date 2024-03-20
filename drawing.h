d53e52d78d4b554df7dddf0562cf42f3d54b5630
Izak Halseide <imitatio-ignotum@protonmail.com>
2024-03-20 17:54:59 -0600

    Add comment for function: drawBlockKindAt

--- drawing.h
+++ drawing.h
@@ -1,10 +1,14 @@
 #ifndef _FACECRAFT_DRAWING_H_DEFINED_
 #define _FACECRAFT_DRAWING_H_DEFINED_
 
+
 #include "raylib/raylib.h"
 
 #include "types.h"
 
+
+// Draw a block kind at a given position, and only draw the faces given by `facesFlags`.
+// For example, the `facesFlags` for back & top would be: `(1 << CUBE_DIRECTION_BACK) | (1 << CUBE_DIRECTION_TOP)`.
 void drawBlockKindAt(const AtlasUVs *textureUVs, BlockKind blockKind, Vector3 centerPos, float size, Color color, long facesFlags);
 
 // Draw cube face textured
